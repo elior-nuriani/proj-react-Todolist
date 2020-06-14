132ce06dfebd0aabf6cb668c219fe531bbb7c9dc
Elior Nuri <eliornuri777@gmail.com>
2020-06-14 15:47:07 +0300

    Updates

---
+++ precache-manifest.b74ae02088f98c50a99984f4cba7a370.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9eb22e0510cf3028ec387f2f6312f8a",
+    "url": "/proj-react-Todolist/index.html"
+  },
+  {
+    "revision": "d8724399f1af92ff6568",
+    "url": "/proj-react-Todolist/static/css/main.b62f206a.chunk.css"
+  },
+  {
+    "revision": "92c484bd719d666c98e3",
+    "url": "/proj-react-Todolist/static/js/2.b5d9f57b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/proj-react-Todolist/static/js/2.b5d9f57b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8724399f1af92ff6568",
+    "url": "/proj-react-Todolist/static/js/main.8244736e.chunk.js"
+  },
+  {
+    "revision": "99a58749290f0ea1983b",
+    "url": "/proj-react-Todolist/static/js/runtime-main.1fc659fe.js"
+  },
+  {
+    "revision": "d0e0c9d174a9cec8f1383ebd7bf51b31",
+    "url": "/proj-react-Todolist/static/media/BalsamiqSans-Regular.d0e0c9d1.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/proj-react-Todolist/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "9942588a6c84f959132556d99e83ded6",
+    "url": "/proj-react-Todolist/static/media/Raleway-Regular.9942588a.ttf"
+  },
+  {
+    "revision": "8e22c2a6e3a3c679787e763a97fa11f7",
+    "url": "/proj-react-Todolist/static/media/Ubuntu-Medium.8e22c2a6.ttf"
+  },
+  {
+    "revision": "2505bfbd9bde14a7829cc8c242a0d25c",
+    "url": "/proj-react-Todolist/static/media/Ubuntu-Regular.2505bfbd.ttf"
+  }
+]);
